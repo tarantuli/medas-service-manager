9c17214900743437eeed9b2b88d2c4a1cced93eb
Patrick Morpey <info@morphp.nl>
2021-10-18 15:04:38 +0200

    Moved BaseException to medas-core

--- src/Exceptions/ServiceNotFoundByTypesException.php
+++ src/Exceptions/ServiceNotFoundByTypesException.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace Medas\ServiceManager\Exceptions;
 
+use Medas\Core\Exceptions\BaseException;
+
 class ServiceNotFoundByTypesException extends BaseException
 {
     public function __construct(array $types)

601715c008ee2861b568351a7f57f362c039cfcc
Patrick Morpey <patrick@morphp.nl>
2022-08-22 12:31:15 +0200

    Added ConfigValue attribute

---
+++ src/ConfigOptions/ConfigValue.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Medas\ServiceManager\ConfigOptions;
+
+/**
+ * Allow both to allow the attribute to be assigned to parameters in constructors
+ * with property promotion.
+ */
+#[\Attribute(\Attribute::TARGET_PARAMETER | \Attribute::TARGET_PROPERTY)]
+class ConfigValue
+{
+    public function __construct(public string $configOption)
+    {
+    }
+}

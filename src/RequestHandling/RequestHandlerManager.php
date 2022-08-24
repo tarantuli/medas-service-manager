f2be476d668e32bb54a7affa80434756c637e9e3
Patrick Morpey <patrick@morphp.nl>
2022-08-24 15:55:09 +0200

    Added findByName()

--- src/RequestHandling/RequestHandlerManager.php
+++ src/RequestHandling/RequestHandlerManager.php
@@ -7,4 +7,6 @@ namespace Medas\ServiceManager\RequestHandling;
 interface RequestHandlerManager
 {
     public function find(string $method, string $path): RequestHandler|null;
+
+    public function findByName(string $name): RequestHandler|null;
 }

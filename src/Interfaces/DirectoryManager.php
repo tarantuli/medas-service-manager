4825ae4256224127e297889352af11d69f484cd6
Patrick Morpey <patrick@morphp.nl>
2023-03-02 15:58:31 +0100

    Require it to be iterable in general

--- src/Interfaces/DirectoryManager.php
+++ src/Interfaces/DirectoryManager.php
@@ -8,9 +8,9 @@ interface DirectoryManager
 {
     public function loadPhpFiles(string $directory): void;
 
-    public function recursiveFindByExtension(string $directory, string $extension): \RegexIterator;
+    public function recursiveFindByExtension(string $directory, string $extension): iterable;
 
-    public function recursiveFind(string $directory, string $pattern): \RegexIterator;
+    public function recursiveFind(string $directory, string $pattern): iterable;
 
     public function create(string $path): void;
 }
